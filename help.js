0e9021ae0905b92997ff9634fd3473ab0166580f
sahupr <sahupr@mail.uc.edu>
2021-01-16 17:29:57 -0500

    countdown timer

--- help.js
+++ help.js
@@ -1,6 +1,6 @@
 function helpCommand(arguments, receivedMessage) {
     if(arguments.length > 0){
-        receivedMessage.channel.send('help required with: ' + arguments)
+        receivedMessage.channel.send('add time in minutes - `!countdown 5`')
     }
     else{
         receivedMessage.channel.send('invalid input, try being more specific: something like `!help [topic]` ')
