66477daa3853aadee380f8272708588bc13e59ca
sahupr <sahupr@mail.uc.edu>
2021-02-11 11:34:47 -0500

    typo

--- checkin.js
+++ checkin.js
@@ -85,7 +85,7 @@ module.exports = async function(message) {
       if(err.response?.status === 404) {
         message.channel.send(`Attendee with email ${censorEmail(email)} does not exist, please make sure you are registered with us or contact an organizer`)
       } else if (err.response?.status === 403) {
-          message.channel.send(`Attendee with email ${censorEmail(email)} is already checked in, are you clone?`)
+          message.channel.send(`Attendee with email ${censorEmail(email)} is already checked in, are you a clone?`)
       }
       else {
         message.channel.send(`Error checking in ${censorEmail(email)}, please try again later or contact an organizer`)
