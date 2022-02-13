546bd7f2fd04c35853b2fa87ad674251069bc6ca
Dev Agrawal <devagrawal09@gmail.com>
2022-02-13 01:02:25 -0500

    fixed datatypes import

--- database/index.js
+++ database/index.js
@@ -1,4 +1,4 @@
-const { Sequelize } = require(`sequelize`);
+const { Sequelize, DataTypes } = require(`sequelize`);
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
